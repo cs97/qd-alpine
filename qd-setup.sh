e1c8264fe03f782e8a0e86780aca44918111186e
cs97 <chrswr97@gmail.com>
2025-10-05 17:44:34 +0200

    Update qd-setup.sh

--- qd-setup.sh
+++ qd-setup.sh
@@ -14,9 +14,9 @@ install_wayland_sway(){
 
 	setup-apkrepos -o
 
-	#apk add elogind polkit-elogind eudev font-dejavu foot grim i3status sway swayidle swaylockd util-linux-login wl-clipboard wmenu xwayland swaybg git firefox
+	#apk add elogind polkit-elogind eudev font-dejavu foot grim i3status sway swayidle swaylockd util-linux-login wl-clipboard wmenu xwayland swaybg firefox
 
-	apk add elogind polkit-elogind eudev font-dejavu foot grim sway swaylockd xwayland swaybg git firefox nvim
+	apk add elogind polkit-elogind eudev font-dejavu foot grim sway swaylockd xwayland swaybg firefox vim
 
 	setup-devd udev
 
